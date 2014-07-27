33ab9331bfc2a3a662df56d3acf60ee0ece945bd
dlarudgus20 <dlarudgus20@naver.com>
2014-07-27 14:37:36 +0900

    modify & add controls (2)

---
+++ IkhWinLib2/CForm.cpp
@@ -0,0 +1,346 @@
+// Copyright (c) 2014, 임경현
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+// 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "stdafx.h"
+#include "IkhWinLib2/CForm.h"
+#include "IkhWinLib2/CLogicError.h"
+
+BEGIN_IKHWINLIB2()
+
+BEGIN_MSGMAP(CForm, CWindow)
+	MSGMAP_WM_SIZE(OnSize)
+END_MSGMAP(CForm, CWindow)
+
+void CForm::OnSize(UINT state, int cx, int cy)
+{
+	_updateLayout();
+}
+
+CForm::Child CForm::AddChild(CWindow *pChild)
+{
+	assert(this != NULL);
+	assert(GetHWND() != NULL);
+
+	if (_findChildInfo(pChild) != m_vtWindow.end())
+		throw CInvalidChildError(L"pChild는 이미 이 폼의 차일드입니다.");
+
+	HWND hParent = GetParent(*pChild);
+	if (hParent != *this)
+	{
+		SetParent(*pChild, *this);
+	}
+
+	m_vtWindow.push_back(
+		std::make_shared<ChildInfo>(pChild, m_vtWindow.end())
+		);
+
+	if (m_vtWindow.size() >= 2)
+	{
+		auto newone = std::prev(m_vtWindow.end());
+		auto oldone = std::prev(newone);
+		(*newone)->ZOrderPrev = oldone;
+		(*oldone)->ZOrderNext = newone;
+	}
+
+	auto it = std::prev(m_vtWindow.end());
+	pChild->evtDestroy += m_vtWindow.back()->efpDestroy.set(
+		[this, it](CWindow *) { m_vtWindow.erase(it); }
+	);
+
+	pChild->evtPosSizeChanged += m_vtWindow.back()->efpSizeChanged.set(
+		[this, it](CWindow *) { if (!m_bLayouting) ApplyMovedChild(*it); }
+	);
+
+	_updateLayout();
+
+	return Child(it);
+}
+
+void CForm::RemoveChild(CForm::Child child)
+{
+	assert(this != NULL);
+	assert(GetHWND() != NULL);
+
+	m_vtWindow.erase(child.m_it);
+
+	_updateLayout();
+}
+
+CForm::Child CForm::FindChild(CWindow *pChild)
+{
+	assert(this != NULL);
+	assert(GetHWND() != NULL);
+
+	auto it = _findChildInfo(pChild);
+	if (it == m_vtWindow.end())
+		throw CInvalidChildError();
+
+	return Child(it);
+}
+
+void CForm::DockChild(CForm::Child child, boost::optional<CForm::Child> target,
+	CForm::Dock dock, int gap)
+{
+	assert(this != NULL);
+	assert(GetHWND() != NULL);
+	assert(0 <= dock && dock < 4);
+
+	auto &d = (*child.m_it)->Dock[dock];
+	d.Gap = gap;
+	if (target)
+	{
+		d.TargetKind = TARGET_CHILD;
+		d.TargetChild = *target->m_it;
+		d.TargetChild_it = target->m_it;
+	}
+	else
+	{
+		d.TargetKind = TARGET_FORM;
+	}
+
+	_updateLayout();
+}
+
+void CForm::MoveChild(CForm::Child child, RECT pos)
+{
+	assert(this != NULL);
+	assert(GetHWND() != NULL);
+
+	(*child.m_it)->PosRect = pos;
+
+	RECT ClientRt;
+	GetClientRect(*this, &ClientRt);
+
+	stlgc::set<std::shared_ptr<ChildInfo> > ModifiedList;
+	RecursiveLayout(*child.m_it, ClientRt, ModifiedList);
+
+	m_bLayouting = true;
+	HDWP hDefer = BeginDeferWindowPos((int)(unsigned)ModifiedList.size());
+	DeferMoveChild(hDefer, *child.m_it);
+	for (const auto &sp : ModifiedList)
+	{
+		DeferMoveChild(hDefer, sp);
+		sp->fProcessed = false;
+	}
+	EndDeferWindowPos(hDefer);
+	m_bLayouting = false;
+}
+
+void CForm::PerformLayout()
+{
+	assert(this != NULL);
+	assert(GetHWND() != NULL);
+
+	RECT ClientRt;
+	GetClientRect(*this, &ClientRt);
+
+	stlgc::set<std::shared_ptr<ChildInfo> > unused;
+	for (const auto &sp : m_vtWindow)
+	{
+		if (!sp->fProcessed)
+		{
+			RecursiveLayout(sp, ClientRt, unused);
+			unused.clear();
+		}
+	}
+
+	m_bLayouting = true;
+	HDWP hDefer = BeginDeferWindowPos((int)(unsigned)m_vtWindow.size());
+	for (const auto &sp : m_vtWindow)
+	{
+		DeferMoveChild(hDefer, sp);
+		sp->fProcessed = false;
+	}
+	EndDeferWindowPos(hDefer);
+	m_bLayouting = false;
+
+	m_bLayoutRequired = false;
+}
+void CForm::DeferMoveChild(HDWP hDefer, const std::shared_ptr<CForm::ChildInfo> &sp)
+{
+	HWND hWndInsertAfter;
+	if (sp->ZOrderPrev != m_vtWindow.end())
+		hWndInsertAfter = (*sp->ZOrderPrev)->pWnd->GetHWND();
+	else
+		hWndInsertAfter = NULL;
+
+	DeferWindowPos(hDefer, *sp->pWnd, hWndInsertAfter,
+		sp->PosRect.left, sp->PosRect.top,
+		sp->PosRect.right - sp->PosRect.left, sp->PosRect.bottom - sp->PosRect.top,
+		SWP_NOACTIVATE | (hWndInsertAfter ? 0 : SWP_NOZORDER));
+}
+
+void CForm::RecursiveLayout(const std::shared_ptr<CForm::ChildInfo> &pci, const RECT &ClientRt,
+	stlgc::set<std::shared_ptr<CForm::ChildInfo> > &ModifiedList)
+{
+	if (pci->fProcessed)
+		return;
+	pci->fProcessed = true;
+
+	GetWindowRect(*pci->pWnd, &pci->PosRect);
+	ScreenToClient(*this, (LPPOINT)&pci->PosRect.left);
+	ScreenToClient(*this, (LPPOINT)&pci->PosRect.right);
+
+	RECT &rt = pci->PosRect;
+
+	bool left_modified = false;
+	bool top_modified = false;
+
+	// TARGET_CHILD
+	if (pci->Dock[DockLeft].TargetKind == TARGET_CHILD)
+	{
+		auto sp = pci->Dock[DockLeft].TargetChild.lock();
+		if (sp)
+		{
+			ModifiedList.insert(sp);
+			RecursiveLayout(sp, ClientRt, ModifiedList);
+			LONG delta = (sp->PosRect.right + pci->Dock[DockLeft].Gap) - rt.left;
+			rt.left += delta;
+			rt.right += delta;
+			left_modified = true;
+		}
+		else
+		{
+			pci->Dock[DockLeft].TargetKind = TARGET_NONE;
+		}
+	}
+	if (pci->Dock[DockTop].TargetKind == TARGET_CHILD)
+	{
+		auto sp = pci->Dock[DockTop].TargetChild.lock();
+		if (sp)
+		{
+			ModifiedList.insert(sp);
+			RecursiveLayout(sp, ClientRt, ModifiedList);
+			LONG delta = (sp->PosRect.bottom + pci->Dock[DockLeft].Gap) - rt.top;
+			rt.top += delta;
+			rt.bottom += delta;
+			top_modified = true;
+		}
+		else
+		{
+			pci->Dock[DockTop].TargetKind = TARGET_NONE;
+		}
+	}
+	if (pci->Dock[DockRight].TargetKind == TARGET_CHILD)
+	{
+		auto sp = pci->Dock[DockRight].TargetChild.lock();
+		if (sp)
+		{
+			ModifiedList.insert(sp);
+			RecursiveLayout(sp, ClientRt, ModifiedList);
+			LONG delta = (sp->PosRect.left - pci->Dock[DockLeft].Gap) - rt.right;
+			rt.right += delta;
+			if (!left_modified)
+				rt.left += delta;
+		}
+		else
+		{
+			pci->Dock[DockRight].TargetKind = TARGET_NONE;
+		}
+	}
+	if (pci->Dock[DockBottom].TargetKind == TARGET_CHILD)
+	{
+		auto sp = pci->Dock[DockBottom].TargetChild.lock();
+		if (sp)
+		{
+			ModifiedList.insert(sp);
+			RecursiveLayout(sp, ClientRt, ModifiedList);
+			LONG delta = (sp->PosRect.top - pci->Dock[DockBottom].Gap) - rt.bottom;
+			rt.bottom += delta;
+			if (!top_modified)
+				rt.top += delta;
+		}
+		else
+		{
+			pci->Dock[DockBottom].TargetKind = TARGET_NONE;
+		}
+	}
+
+	// TARGET_FORM
+	if (pci->Dock[DockLeft].TargetKind == TARGET_FORM)
+	{
+		LONG delta = pci->Dock[DockLeft].Gap - rt.left;
+		rt.left = pci->Dock[DockLeft].Gap;
+		rt.right += delta;
+		left_modified = true;
+	}
+	if (pci->Dock[DockTop].TargetKind == TARGET_FORM)
+	{
+		LONG delta = pci->Dock[DockTop].Gap - rt.top;
+		rt.top = pci->Dock[DockTop].Gap;
+		rt.bottom += delta;
+		top_modified = true;
+	}
+	if (pci->Dock[DockRight].TargetKind == TARGET_FORM)
+	{
+		LONG delta = (ClientRt.right - pci->Dock[DockRight].Gap) - rt.right;
+		rt.right += delta;
+		if (!left_modified)
+			rt.left += delta;
+	}
+	if (pci->Dock[DockBottom].TargetKind == TARGET_FORM)
+	{
+		LONG delta = (ClientRt.bottom - pci->Dock[DockBottom].Gap) - rt.bottom;
+		rt.bottom += delta;
+		if (!top_modified)
+			rt.top += delta;
+	}
+}
+
+void CForm::ApplyMovedChild(const std::shared_ptr<CForm::ChildInfo> &pci)
+{
+	RECT ClientRt;
+	GetClientRect(*this, &ClientRt);
+
+	stlgc::set<std::shared_ptr<ChildInfo> > ModifiedList;
+	RecursiveLayout(pci, ClientRt, ModifiedList);
+
+	m_bLayouting = true;
+	HDWP hDefer = BeginDeferWindowPos((int)(unsigned)ModifiedList.size());
+	for (const auto &sp : ModifiedList)
+	{
+		DeferMoveChild(hDefer, sp);
+		sp->fProcessed = false;
+	}
+	EndDeferWindowPos(hDefer);
+	m_bLayouting = false;
+}
+
+
+// CForm::Child
+CForm::Child CForm::Child::GetDockTarget(Dock d) const
+{
+	if (GetDockKind(d) == TARGET_NONE)
+		throw CInvalidArgError(L"도킹되어 있지 않습니다.");
+	if (GetDockKind(d) == TARGET_FORM)
+		throw CInvalidArgError(L"폼에 도킹되어 있습니다.");
+
+	auto sp = (*m_it)->Dock[d].TargetChild.lock();
+	if (!sp)
+		throw CInvalidArgError(L"도킹한 다른 차일드가 이미 파괴되어 있습니다.");
+
+	return Child(sp->Dock[d].TargetChild_it);
+}
+
+END_IKHWINLIB2()
---
+++ IkhWinLib2/IkhWinLib2/CForm.h
@@ -0,0 +1,181 @@
+// Copyright (c) 2014, 임경현
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+// 
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+/**
+ * @file CForm.h
+ * @brief CForm 클래스
+ */
+
+#pragma once
+
+#include "CWindow.h"
+
+BEGIN_IKHWINLIB2()
+
+class CForm : public CWindow
+{
+	DECLARE_MSGMAP();
+public:
+	enum TargetKind_t { TARGET_NONE, TARGET_CHILD, TARGET_FORM };
+private:
+	struct ChildInfo;
+
+	typedef stlgc::list<std::shared_ptr<ChildInfo> > ChildList;
+	typedef ChildList::iterator ChildIterator;
+
+	struct ChildInfo
+	{
+		struct DockInfo
+		{
+			TargetKind_t TargetKind = TARGET_NONE;
+			std::weak_ptr<ChildInfo> TargetChild;
+			ChildIterator TargetChild_it;
+			int Gap;
+		};
+
+		CWindow *pWnd;
+
+		DockInfo Dock[4];
+
+		EventFnPtr<void(CWindow *)> efpDestroy;
+		EventFnPtr<void(CWindow *)> efpSizeChanged;
+		explicit ChildInfo(CWindow *wnd, ChildIterator end)
+			: pWnd(wnd), fProcessed(false)
+			, it_end(end), ZOrderPrev(end), ZOrderNext(end)
+		{
+		}
+		~ChildInfo()
+		{
+			if (ZOrderPrev != it_end)
+				(*ZOrderPrev)->ZOrderNext = ZOrderNext;
+			if (ZOrderNext != it_end)
+				(*ZOrderNext)->ZOrderPrev = ZOrderPrev;
+		}
+
+		bool fProcessed;
+
+		RECT PosRect;
+
+		ChildIterator it_end;
+		ChildIterator ZOrderPrev, ZOrderNext;
+	};
+
+	ChildList m_vtWindow;
+
+	bool m_bLayoutIsSuspended = false;
+	bool m_bLayoutRequired = false;
+	bool m_bLayouting = false;
+
+public:
+	enum Dock
+	{
+		DockLeft = 0, DockTop = 1, DockRight = 2, DockBottom = 3
+	};
+
+	class Child
+	{
+		friend CForm;
+	private:
+		ChildIterator m_it;
+		Child(ChildIterator it) : m_it(it) { }
+	public:
+		CWindow *GetWnd() const { return (*m_it)->pWnd; }
+
+		TargetKind_t GetDockKind(Dock d) const { return (*m_it)->Dock[d].TargetKind; }
+		Child GetDockTarget(Dock d) const;
+		Child GetZOrderPrev() const { return Child((*m_it)->ZOrderPrev); }
+		Child GetZOrderNext() const { return Child((*m_it)->ZOrderNext); }
+	};
+
+	Child AddChild(CWindow *pWnd);
+	void RemoveChild(Child child);
+	Child FindChild(CWindow *pWnd);
+	typedef boost::optional<Child> TargetForm;
+	void DockChild(Child child, boost::optional<Child> target, Dock dock, int gap);
+	void MoveChild(Child child, RECT pos);
+
+	void SuspendLayout();
+	void ResumeLayout();
+	void PerformLayout();
+private:
+	void _updateLayout();
+	void DeferMoveChild(HDWP hDefer, const std::shared_ptr<ChildInfo> &sp);
+	void ApplyMovedChild(const std::shared_ptr<ChildInfo> &pci);
+
+	template <typename T>
+	struct value_less
+	{
+		bool operator ()(const std::shared_ptr<T> &lhs, const std::shared_ptr<T> &rhs) const
+		{
+			return lhs->get() < rhs->get();
+		}
+	};
+	typedef stlgc::set<std::shared_ptr<ChildInfo>, value_less<ChildInfo> > ModifiedList_t;
+
+	void RecursiveLayout(const std::shared_ptr<ChildInfo> &pci, const RECT &ClientRt,
+		stlgc::set<std::shared_ptr<ChildInfo> > &ModifiedList);
+
+	ChildIterator _findChildInfo(CWindow *pChild);
+
+protected:
+	void OnSize(UINT state, int cx, int cy);
+};
+
+inline void CForm::SuspendLayout()
+{
+	assert(this != NULL);
+	assert(GetHWND() != NULL);
+
+	m_bLayoutIsSuspended = true;
+}
+inline void CForm::ResumeLayout()
+{
+	assert(this != NULL);
+	assert(GetHWND() != NULL);
+
+	m_bLayoutIsSuspended = false;
+	if (m_bLayoutRequired)
+	{
+		PerformLayout();
+	}
+}
+
+inline void CForm::_updateLayout()
+{
+	assert(this != NULL);
+	assert(GetHWND() != NULL);
+
+	if (!m_bLayoutIsSuspended)
+		PerformLayout();
+	else
+		m_bLayoutRequired = true;
+}
+
+inline CForm::ChildIterator CForm::_findChildInfo(CWindow *pChild)
+{
+	auto f = [pChild](const std::shared_ptr<ChildInfo> &sp) { return sp->pWnd == pChild; };
+	return std::find_if(m_vtWindow.begin(), m_vtWindow.end(), f);
+}
+
+END_IKHWINLIB2()
